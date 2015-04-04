f9deb18755815cd2c11eee5daf88e655ea58ebd1
Yoshinobu Wakamatsu <yoshinobu_wk@volare.jp>
2015-04-04 23:05:23 +0900

    display active tasks.

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
+var todos = [{
+  id: '_1',
+  name: 'eat a pizza!',
+  done: true 
+}, {
+  id: '_2',
+  name: 'develop new wheel',
+  done: false
+}, {
+  id: '_3',
+  name: 'discuss bikeshed',
+  done: false
+}];
+
+var Todo = React.createClass({
+  render: function() {
+    var todo = this.props.todo;
+    return (<li className="list-group-item">{todo.name} <button className="btn btn-default btn-xs">Done <span className="glyphicon glyphicon-ok" aria-hidden="true"></span></button></li>);
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    var rows = this.props.todos.filter(function(todo) {
+      return !todo.done;
+    }).map(function(todo) {
+      return (<Todo key={todo.id} todo={todo}></Todo>);
+    });
+    return (
+      <div className="active-todos">
+        <h2>Todo tasks</h2>
+        <ul className="list-group">{rows}</ul>
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="container">
+        <h1>My Todo</h1>
+        <TodoList todos={todos}/>
+      </div>
+    );
+  }
+});
+
 React.render(
-  <h1>Hello, react!</h1>,
+  <App></App>,
   document.getElementById('example')
 );
